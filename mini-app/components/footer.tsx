3f06a24fa41a0e857d354c0fb34cc53b49277425
Plopmenz <plopmenz@gmail.com>
2025-10-14 17:53:31 +0400

    update footer

--- mini-app/components/footer.tsx
+++ mini-app/components/footer.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import Link from "next/link";
+import { useMiniAppContext } from "./context/miniapp-provider";
 
 export function Footer() {
+  const { sdk, isInMiniApp } = useMiniAppContext();
+
   return (
     <footer className="flex place-content-center pb-4">
       <span>
-        Developed by{" "}
-        <Link href="https://openxai.org" target="_blank">
-          OpenxAI
-        </Link>
+        Made with{" "}
+        {isInMiniApp ? (
+          <span
+            onClick={() => {
+              sdk.actions.openMiniApp({
+                url: "https://miniapp-factory.marketplace.openxai.network",
+              });
+            }}
+          >
+            Mini App Factory
+          </span>
+        ) : (
+          <Link
+            href="https://miniapp-factory.marketplace.openxai.network"
+            target="_blank"
+          >
+            Mini App Factory
+          </Link>
+        )}
       </span>
     </footer>
   );
